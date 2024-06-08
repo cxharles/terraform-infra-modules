1141dc93598b98c30a5299210c149c53f5001538
cxharles <121370900+cxharles@users.noreply.github.com>
2024-06-08 17:36:11 -0400

    Create main.tf

---
+++ dev-infra/main.tf
@@ -0,0 +1,23 @@
+data "vault_generic_secret" "aws_creds" {
+  path = "kv/modules-secret"
+}
+
+provider "aws" {
+  region     = data.vault_generic_secret.aws_creds.data["region"]
+  access_key = data.vault_generic_secret.aws_creds.data["aws_access_key_id"]
+  secret_key = data.vault_generic_secret.aws_creds.data["aws_secret_access_key"]
+}
+
+# create vpc
+module "vpc" {
+  source                       = "../modules/vpc"
+  region                       = var.region
+  project_name                 = var.project_name
+  vpc_cidr                     = var.vpc_cidr
+  public_subnet_az1_cidr       = var.public_subnet_az1_cidr
+  public_subnet_az2_cidr       = var.public_subnet_az2_cidr
+  private_app_subnet_az1_cidr  = var.private_app_subnet_az1_cidr
+  private_app_subnet_az2_cidr  = var.private_app_subnet_az2_cidr
+  private_data_subnet_az1_cidr = var.private_data_subnet_az1_cidr
+  private_data_subnet_az2_cidr = var.private_data_subnet_az2_cidr
+}
